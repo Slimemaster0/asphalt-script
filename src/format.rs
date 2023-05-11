0415252f52ef0748bf95138f02f8def5fa6df823
Slimemaster <milo@munksgaard.eu>
2023-05-11 15:55:05 +0200

    Add file for text formatting codes.

---
+++ src/format.rs
@@ -0,0 +1,35 @@
+#[allow(unused)]
+pub const RESET_FORMAT: &str =   "\x1b[0m";
+
+#[allow(unused)]
+pub const BLACK: &str =  "\x1b[30m";
+#[allow(unused)]
+pub const RED: &str =     "\x1b[31m";
+#[allow(unused)]
+pub const GREEN: &str =   "\x1b[32m";
+#[allow(unused)]
+pub const YELLOW: &str =  "\x1b[33m";
+#[allow(unused)]
+pub const BLUE: &str =    "\x1b[34m";
+#[allow(unused)]
+pub const MAGENTA: &str = "\x1b[35m";
+#[allow(unused)]
+pub const CYAN: &str =    "\x1b[36m";
+
+#[allow(unused)]
+pub const BLACK_BG: &str =   "\x1b[40m";
+#[allow(unused)]
+pub const RED_BG: &str =     "\x1b[41m";
+#[allow(unused)]
+pub const GREEN_BG: &str =   "\x1b[42m";
+#[allow(unused)]
+pub const YELLOW_BG: &str =  "\x1b[43m";
+#[allow(unused)]
+pub const BLUE_BG: &str =    "\x1b[44m";
+#[allow(unused)]
+pub const MAGENTA_BG: &str = "\x1b[45m";
+#[allow(unused)]
+pub const CYAN_BG: &str =    "\x1b[46m";
+
+#[allow(unused)]
+pub const BOLD: &str =     "\x1b[1m";
