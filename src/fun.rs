9e20edb279649d5005aca163a576237e6bee75e8
Slimemaster <milo@munksgaard.eu>
2023-05-20 17:15:26 +0200

    Add new function 'del' that deletes a variable.

--- src/fun.rs
+++ src/fun.rs
@@ -12,14 +12,15 @@ use crate::format::*;
 use crate::read::read_to_string;
 
 pub fn fun(input: &str, stack: &mut Vec<Item>) -> Value {
+    if input.contains("(") && input.contains(")") {
     let keyword: Vec<&str> = input.split("(").collect();
-    if keyword.len() != 1 {
         match keyword[0].to_owned().as_str().trim() {
             "printf" => printf(parse_args(input, stack)),
             "new" => new_var(parse_args(input, stack), stack),
             "test_parse_args" => test_parse_args(parse_args(input, stack)),
             "die" => die(parse_args(input, stack)),
             "readf" => return read_to_string(parse_args(input, stack)),
+            "del" => del_var(parse_args(input, stack), stack),
 
             _ => eprintln!("\x1b[31mERR:\x1b[0m {}", input),
         }
@@ -45,17 +46,73 @@ pub fn parse_args(str: &str, stack: &mut Vec<Item>) -> Vec<Value> { // {{{
         .expect("Could not remove first char")
         .to_string();
 
-    let mut args_str: Vec<String> = string // Split the string
-        .split(",")
-        .map(|s| s.to_string()).collect();
 
-    for i in 0..args_str.len() { // Trim each argument
-        args_str[i] = args_str[i].trim().to_string();
+    match env::var("DEBUG_PARSER") {
+        Ok(v) => {
+            if v == "1" {
+                eprintln!("Arguments string: {string}");
+            }
+        }
+        Err(_) => {}
     }
+
+    // {{{ Split the argument string
+    let mut is_str: bool = false;
+    let mut parens: usize = 0;
+    let mut split_points: Vec<usize> = Vec::new();
+    let chars: Vec<char> = string.chars().collect();
+
+    for i in 0..chars.len() {
+        if chars[i] == '"' {
+            if is_str {
+                is_str = false;
+            } else {
+                is_str = true;
+            }
+            continue;
+        }
+
+        if chars[i] == '(' {
+            parens += 1;
+        }
+        if chars[i] == ')' {
+            parens -= 1;
+        }
+
+        if parens == 0 && is_str == false && chars[i] == ',' {
+            split_points.push(i);
+        }
+    }
+
+    let mut args_str: Vec<String> = split_muti_points(&string, &split_points);
+
+    for i in 0..args_str.len() {
+        if args_str[i].chars().nth(0).expect("{RED}Err:{RESET_FORMAT} No char at 0") == ',' {
+            let rm_first = remove_first(args_str[i].as_str()).expect("{RED}Err:{RESET_FORMAT} Could not remove first!");
+            args_str[i] = rm_first.trim().to_string();
+            continue;
+        } else {
+            args_str[i] = args_str[i].trim().to_string();
+        }
+    }
+
+    // }}}
+
+    match env::var("DEBUG_PARSER") {
+        Ok(v) => {
+            if v == "1" {
+                for i in 0..args_str.len() {
+                    eprintln!("Arg{}: {}", i, args_str[i]);
+                }
+            }
+        }
+        Err(_) => {}
+    }
+
 
     let mut args: Vec<Value> = Vec::new();
 
-    for i in 0..args_str.len() { // {{{ parse each arguments
+    for i in 0..args_str.len() { // {{{ parse each argument
 
         if args_str[i].contains("(") && args_str[i].chars().nth(args_str[i].len() -1).expect("No char at {args_str[i] -1}") == ')' { // function
             let r = &mut *stack;
@@ -154,4 +211,24 @@ pub fn parse_args(str: &str, stack: &mut Vec<Item>) -> Vec<Value> { // {{{
 
 fn remove_first(s: &str) -> Option<&str> {
     s.chars().next().map(|c| &s[c.len_utf8()..])
+} // }}}
+
+fn split_muti_points(str: &String, points: &Vec<usize>) -> Vec<String> { // {{{
+    let mut return_val: Vec<String> = Vec::new();
+
+    if points.len() == 0 {
+        return_val.push(str.to_owned());
+        return return_val;
+    } else {
+        return_val.push(str.split_at(points[0]).0.to_string());
+    }
+
+    for i in 0..points.len() -1 {
+        return_val.push(str.split_at(points[i]).1.split_at(points[i+1] - points[i]).0.to_string());
+    }
+
+    return_val.push(str.split_at(points[points.len() -1]).1.to_string());
+
+
+    return return_val;
 } // }}}
--- src/memory.rs
+++ src/memory.rs
@@ -2,6 +2,7 @@
 
 use std::process::exit;
 use crate::errcodes::*;
+use crate::format::*;
 
 #[derive(Clone)]
 pub enum Value {
@@ -30,7 +31,7 @@ pub fn read_pointer(stack: &Vec<Item>, name: &str) -> usize { // {{{
 
 pub fn new_var(args: Vec<Value>, stack: &mut Vec<Item>) { // {{{
     if args.len() != 2 {
-        eprintln!("\x1b31mErr: 'new' takes in 2 arguments");
+        eprintln!("\x1b31mErr:\x1b0m 'new' takes in 2 arguments");
         exit(BAD_ARGC);
     }
 
@@ -45,3 +46,22 @@ pub fn new_var(args: Vec<Value>, stack: &mut Vec<Item>) { // {{{
         }
     }
 } // }}}
+
+pub fn del_var(args: Vec<Value>, stack: &mut Vec<Item>) {
+    if args.len() != 1 {
+        eprintln!("{RED}Err:{RESET_FORMAT} 'del' takes in 1 argument");
+        exit(BAD_ARGC);
+    }
+
+    match &args[0] {
+        Value::String(i) => {
+            stack.remove(read_pointer(&stack.clone(), i.as_str()));
+        }
+
+        _ => {
+            eprintln!("\x1b[31mErr:\x1b[0m The first argument for 'del' must be a 'String'");
+            exit(WRONG_ARGT);
+        }
+
+    }
+}
